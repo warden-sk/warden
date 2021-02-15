b9171af685bc2b7306fe50e0f87242ae6d7b8312
Marek Kobida <marek.kobida@gmail.com>
2021-02-15 23:06:31 +0100

    update

--- private/index.js
+++ private/index.js
@@ -8,4 +8,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
 const react_dom_1 = __importDefault(require("react-dom"));
-react_dom_1.default.render(react_1.default.createElement("h1", null, "warden.sk"), document.getElementById('index'));
+react_dom_1.default.render(react_1.default.createElement("div", { className: "container" },
+    react_1.default.createElement("div", null,
+        react_1.default.createElement("h1", { className: "name" }, "warden.sk"),
+        react_1.default.createElement("p", { className: "description" }, "\u010Coskoro v\u00E1m predstav\u00EDm str\u00E1nku v \u00FAplne novom kab\u00E1te."))), document.getElementById('index'));
