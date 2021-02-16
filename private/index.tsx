a4910b11e89e160bc4b7202f2c274d8641007420
Marek Kobida <marek.kobida@gmail.com>
2021-02-16 23:36:57 +0100

    update

--- private/index.tsx
+++ private/index.tsx
@@ -2,7 +2,33 @@
  * Copyright 2021 Marek Kobida
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import Router from '@warden-sk/router/Router';
 
-ReactDOM.render(<h1>warden.sk</h1>, document.getElementById('index'));
+const router = new Router<[(element: JSX.Element) => any]>();
+
+router.addRoute('/test').get(({}, writeElement) => writeElement(<h1>test</h1>));
+
+function Index(): JSX.Element {
+  const [readElement, writeElement] = useState<JSX.Element>(
+    <>
+      <h1>warden.sk</h1>
+      <a href="?test#/test">test</a>
+    </>
+  );
+
+  function test() {
+    router.test([writeElement], 'GET', location.hash.substring(1));
+  }
+
+  useEffect(() => {
+    test();
+
+    window.addEventListener('hashchange', test, false);
+  }, []);
+
+  return readElement;
+}
+
+ReactDOM.render(<Index />, document.getElementById('index'));
