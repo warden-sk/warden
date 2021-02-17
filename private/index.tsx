708eb954615dfc610cadec2373d4d8846a2e6dcd
Marek Kobida <marek.kobida@gmail.com>
2021-02-17 11:14:14 +0100

    update

--- private/index.tsx
+++ private/index.tsx
@@ -8,18 +8,24 @@ import Router from '@warden-sk/router/Router';
 
 const router = new Router<[(element: JSX.Element) => any]>();
 
+router.addRoute('/').get(({}, writeElement) =>
+  writeElement(
+    <>
+      <h1>warden.sk</h1>
+      <a href="#/test">test</a>
+    </>
+  )
+);
+
 router.addRoute('/test').get(({}, writeElement) => writeElement(<h1>test</h1>));
 
 function Index(): JSX.Element {
-  const [readElement, writeElement] = useState<JSX.Element>(
-    <>
-      <h1>warden.sk</h1>
-      <a href="?test#/test">test</a>
-    </>
-  );
+  const [readElement, writeElement] = useState<JSX.Element>(<h1 />);
 
-  function test() {
-    router.test([writeElement], 'GET', location.hash.substring(1));
+  function test(event?: HashChangeEvent): any {
+    const url = location.hash.substring(1) || '/';
+
+    router.test([writeElement], 'GET', url);
   }
 
   useEffect(() => {
