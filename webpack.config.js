f04b0fb03133d19ab14581dca565efcb5bd228aa
Marek Kobida <marek.kobida@gmail.com>
2021-02-18 23:17:16 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const path = require('path');
 
 module.exports = {
   entry: './compiled/index.js',
-  mode: 'production',
   output: {
     filename: 'index.compiled.js',
     path: path.resolve(__dirname, './compiled'),
