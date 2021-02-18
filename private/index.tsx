3f2c5f67d62942895ad009ea3b6ba0333aaeca58
Marek Kobida <marek.kobida@gmail.com>
2021-02-18 15:59:53 +0100

    updated router

--- private/index.tsx
+++ private/index.tsx
@@ -2,20 +2,27 @@
  * Copyright 2021 Marek Kobida
  */
 
-import Link from '@warden-sk/router/components/Link';
+import Link from '@warden-sk/react-router/Link';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Route from '@warden-sk/router/components/Route';
-import Router from '@warden-sk/router/components/Router';
+import Route from '@warden-sk/react-router/Route';
+import Router from '@warden-sk/react-router/Router';
+import urlParameters from '@warden-sk/react-router/urlParameters';
+
+function Test() {
+  const { id } = urlParameters();
+
+  return <h1>Test {id}</h1>;
+}
 
 ReactDOM.render(
   <Router>
     <Route path="/">
       <h1>warden.sk</h1>
-      <Link to="/test">➡️</Link>
+      <Link to="/test/1">➡️</Link>
     </Route>
-    <Route path="/test">
-      <h1>test</h1>
+    <Route path="/test/:id">
+      <Test />
     </Route>
   </Router>,
   document.getElementById('index')
