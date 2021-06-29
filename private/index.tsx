a09cc2c6f5f41b290aad31732a4f00ba8a899edf
Marek Kobida <marek.kobida@gmail.com>
2021-06-29 11:31:50 +0200

    update

--- private/index.tsx
+++ private/index.tsx
@@ -2,41 +2,31 @@
  * Copyright 2021 Marek Kobida
  */
 
-import Link from '@warden-sk/react-router/Link';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Route from '@warden-sk/react-router/Route';
-import Router from '@warden-sk/react-router/Router';
-import currentUrlParameters from '@warden-sk/react-router/currentUrlParameters';
-
-function _2() {
-  const { id } = currentUrlParameters();
-
-  return (
-    <div className="container">
-      <div>
-        <h1>{id}</h1>
-        <Link url="/">späť️️</Link>
-      </div>
-    </div>
-  );
-}
 
 function _1() {
   return (
-    <Router>
-      <Route url="/">
-        <div className="container">
-          <div>
-            <h1>warden.sk</h1>
-            <p>Čoskoro vám predstavím stránku v úplne novom kabáte.</p>
-          </div>
+    <div className="container">
+      <div>
+        <h1>warden.sk</h1>
+        <p className="_1">Čoskoro vám predstavím stránku v úplne novom kabáte.</p>
+        <div className="contact">
+          <h2>Kontakt</h2>
+          <p>
+            Marek Kobida
+            <br />
+            <a href="#">+421 910 123 678</a>
+            <br />
+            <a href="#">marek@warden.sk</a>
+          </p>
+          <h3>Právnik</h3>
+          <p>
+            <a href="#">lawyer@warden.sk</a>
+          </p>
         </div>
-      </Route>
-      <Route url="/test/:id">
-        <_2 />
-      </Route>
-    </Router>
+      </div>
+    </div>
   );
 }
 
