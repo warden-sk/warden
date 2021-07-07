de15c68ebd69de211e0af6ac58c202c55401108c
Marek Kobida <marek.kobida@gmail.com>
2021-07-07 13:43:31 +0200

    update

--- private/index.tsx
+++ private/index.tsx
@@ -8,38 +8,40 @@ import ReactDOM from 'react-dom';
 function _1() {
   return (
     <div className="container">
-      <div>
-        <h1>warden.sk</h1>
-        <p className="_1">Čoskoro vám predstavíme stránku v úplne novom kabáte.</p>
-        <div className="contact">
-          <h2>Kontakt</h2>
-          <p>
-            Marek Kobida
-            <br />
-            <a href="tel:+421910123678">+421 910 123 678</a>
-            <br />
-            <a href="mailto:marek@warden.sk">marek@warden.sk</a>
-          </p>
-          <h3>Právny zástupca</h3>
-          <p>
-            <a href="mailto:lawyer@warden.sk">lawyer@warden.sk</a>
-          </p>
-          <h3>Bankové údaje</h3>
-          <p>
-            Slovenská sporiteľňa, a. s.
-            <br />
-            IBAN:{' '}
-            <span contentEditable onKeyDown={e => (e.metaKey ? true : e.preventDefault())}>
-              SK51 0900 0000 0050 7145 4547
-            </span>
-          </p>
-          <div className="buttons">
-            <a className="discord" href="https://discord.gg/zH8GKfErWb" target="_blank">
-              pripojiť sa k sieti Discord
-            </a>
-            <a className="github" href="https://github.com/warden-sk/warden" target="_blank">
-              upraviť stránku
-            </a>
+      <div className="_1">
+        <div className="_2">
+          <h1>warden.sk</h1>
+          <p className="_3">Čoskoro vám predstavíme stránku v úplne novom kabáte.</p>
+          <div className="contact">
+            <h2>Kontakt</h2>
+            <p>
+              Marek Kobida
+              <br />
+              <a href="tel:+421910123678">+421 910 123 678</a>
+              <br />
+              <a href="mailto:marek@warden.sk">marek@warden.sk</a>
+            </p>
+            <h3>Právny zástupca</h3>
+            <p>
+              <a href="mailto:lawyer@warden.sk">lawyer@warden.sk</a>
+            </p>
+            <h3>Bankové údaje</h3>
+            <p>
+              Slovenská sporiteľňa, a. s.
+              <br />
+              IBAN:{' '}
+              <span contentEditable onKeyDown={e => (e.metaKey ? true : e.preventDefault())}>
+                SK51 0900 0000 0050 7145 4547
+              </span>
+            </p>
+            <div className="buttons">
+              <a className="discord" href="https://discord.gg/zH8GKfErWb" target="_blank">
+                pripojiť sa k sieti Discord
+              </a>
+              <a className="github" href="https://github.com/warden-sk/warden" target="_blank">
+                upraviť stránku
+              </a>
+            </div>
           </div>
         </div>
       </div>
