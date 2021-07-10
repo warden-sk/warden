b0a7310920071afbe7e765836d7a9ed648536b84
Marek Kobida <marek.kobida@gmail.com>
2021-07-10 14:47:38 +0200

    up

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 function _1() {
-    return (React.createElement("div", { className: "container" },
-        React.createElement("div", { className: "_1 p-2" },
-            React.createElement("div", { className: "_2 p-8" },
+    return (React.createElement("div", { className: "_1" },
+        React.createElement("div", { className: "_2 p-2" },
+            React.createElement("div", { className: "_3 p-8" },
                 React.createElement("h1", { className: "m-b-4" }, "warden.sk"),
-                React.createElement("p", { className: "_3 m-b-4" }, "\u010Coskoro v\u00E1m predstav\u00EDme str\u00E1nku v \u00FAplne novom kab\u00E1te."),
+                React.createElement("p", { className: "m-b-4" }, "\u010Coskoro v\u00E1m predstav\u00EDme str\u00E1nku v \u00FAplne novom kab\u00E1te."),
                 React.createElement("h2", null, "Kontakt"),
                 React.createElement("p", { className: "m-b-4" },
                     "Marek Kobida",
@@ -20,14 +20,11 @@ function _1() {
                 React.createElement("p", { className: "m-b-4" },
                     React.createElement("a", { href: "mailto:lawyer@warden.sk" }, "lawyer@warden.sk")),
                 React.createElement("h3", null, "Bankov\u00E9 \u00FAdaje"),
-                React.createElement("p", { className: "m-b-4" },
+                React.createElement("p", null,
                     "Slovensk\u00E1 sporite\u013E\u0148a, a. s.",
                     React.createElement("br", null),
                     "IBAN:",
                     ' ',
-                    React.createElement("span", { contentEditable: true, onKeyDown: e => (e.metaKey ? true : e.preventDefault()) }, "SK51 0900 0000 0050 7145 4547")),
-                React.createElement("div", { className: "buttons" },
-                    React.createElement("a", { className: "discord", href: "https://discord.gg/zH8GKfErWb", target: "_blank" }, "pripoji\u0165 sa k sieti Discord"),
-                    React.createElement("a", { className: "github", href: "https://github.com/warden-sk/warden", target: "_blank" }, "upravi\u0165 str\u00E1nku"))))));
+                    React.createElement("span", { contentEditable: true, onKeyDown: e => (e.metaKey ? true : e.preventDefault()) }, "SK51 0900 0000 0050 7145 4547"))))));
 }
 ReactDOM.render(React.createElement(_1, null), document.getElementById('index'));
