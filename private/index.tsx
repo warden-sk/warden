44ecacb6e84ce22cbd7e379f5780418209b62e11
Marek Kobida <marek.kobida@gmail.com>
2021-07-10 14:47:40 +0200

    up

--- private/index.tsx
+++ private/index.tsx
@@ -7,11 +7,11 @@ import ReactDOM from 'react-dom';
 
 function _1() {
   return (
-    <div className="container">
-      <div className="_1 p-2">
-        <div className="_2 p-8">
+    <div className="_1">
+      <div className="_2 p-2">
+        <div className="_3 p-8">
           <h1 className="m-b-4">warden.sk</h1>
-          <p className="_3 m-b-4">Čoskoro vám predstavíme stránku v úplne novom kabáte.</p>
+          <p className="m-b-4">Čoskoro vám predstavíme stránku v úplne novom kabáte.</p>
           <h2>Kontakt</h2>
           <p className="m-b-4">
             Marek Kobida
@@ -25,7 +25,7 @@ function _1() {
             <a href="mailto:lawyer@warden.sk">lawyer@warden.sk</a>
           </p>
           <h3>Bankové údaje</h3>
-          <p className="m-b-4">
+          <p>
             Slovenská sporiteľňa, a. s.
             <br />
             IBAN:{' '}
@@ -33,14 +33,6 @@ function _1() {
               SK51 0900 0000 0050 7145 4547
             </span>
           </p>
-          <div className="buttons">
-            <a className="discord" href="https://discord.gg/zH8GKfErWb" target="_blank">
-              pripojiť sa k sieti Discord
-            </a>
-            <a className="github" href="https://github.com/warden-sk/warden" target="_blank">
-              upraviť stránku
-            </a>
-          </div>
         </div>
       </div>
     </div>
