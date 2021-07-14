bfce7cb33c12625e647f4b9f5bd78ebab156f1e9
Marek Kobida <marek.kobida@gmail.com>
2021-07-14 16:12:16 +0200

    rollup instead of webpack

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+
+export default {
+  input: './public/index.js',
+  output: {
+    file: './public/index.compiled.js',
+    format: 'iife',
+  },
+  plugins: [
+    commonjs(),
+    nodeResolve({
+      extensions: ['.js', '.ts', '.tsx'],
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+  ],
+};
