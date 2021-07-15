198aff8cb9ffe86ef5d4fff8e1ab2e860bf5fc71
Marek Kobida <marek.kobida@gmail.com>
2021-07-15 11:44:16 +0200

    terser

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,20 @@
  */
 
 import commonjs from '@rollup/plugin-commonjs';
+import { minify } from 'terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import typescript from '@rollup/plugin-typescript';
+
+function terser() {
+  return {
+    name: 'terser',
+    async renderChunk(code) {
+      const { code: c } = await minify(code);
+      return c;
+    },
+  };
+}
 
 export default {
   input: './private/index.tsx',
@@ -21,5 +32,7 @@ export default {
       extensions: ['.js', '.ts', '.tsx'],
     }),
     commonjs(),
+
+    terser(),
   ],
 };
