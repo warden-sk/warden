901eae2331950b558d40d86d9ffaf0c7daf07821
Marek Kobida <marek.kobida@gmail.com>
2021-08-17 15:49:43 +0200

    webpack watch

--- webpack.js
+++ webpack.js
@@ -12,33 +12,32 @@ const OUTPUT_FILE_NAME = 'index.js';
 
 const OUTPUT_FILE_PATH = './public';
 
-webpack(
-  {
-    entry: INPUT_FILE_PATH,
-    mode: 'production',
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          type: 'asset/resource',
-        },
-        {
-          exclude: /node_modules/,
-          loader: './webpack/babel',
-          test: /\.tsx?$/,
-        },
-      ],
-    },
-    name: 'warden',
-    output: {
-      assetModuleFilename: '[name][ext]',
-      filename: OUTPUT_FILE_NAME,
-      path: path.resolve(OUTPUT_FILE_PATH),
-    },
-    plugins: [new Html()],
-    resolve: {
-      extensions: ['.js', '.json', '.ts', '.tsx'],
-    },
+const compiler = webpack({
+  entry: INPUT_FILE_PATH,
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        type: 'asset/resource',
+      },
+      {
+        exclude: /node_modules/,
+        loader: './webpack/babel',
+        test: /\.tsx?$/,
+      },
+    ],
   },
-  (_, __) => console.log(_, __?.toString({ colors: true }))
-);
+  name: 'warden',
+  output: {
+    assetModuleFilename: '[name][ext]',
+    filename: OUTPUT_FILE_NAME,
+    path: path.resolve(OUTPUT_FILE_PATH),
+  },
+  plugins: [new Html()],
+  resolve: {
+    extensions: ['.js', '.json', '.ts', '.tsx'],
+  },
+});
+
+compiler.watch({}, (_, __) => console.log(_, __?.toString({ colors: true })));
