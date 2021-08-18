31b753e1f3c60b40be3be3c42ab176338274b39c
Marek Kobida <marek.kobida@gmail.com>
2021-08-18 15:07:59 +0200

    fix

--- webpack/babel.ts
+++ webpack/babel.ts
@@ -2,10 +2,12 @@
  * Copyright 2021 Marek Kobida
  */
 
+// @ts-ignore
 import * as babel from '@babel/core';
 
 export default function (code: string): string {
   ({ code } = babel.transformSync(code, {
+    // @ts-ignore
     filename: this.resourcePath,
     presets: ['@babel/preset-react', '@babel/preset-typescript'],
   }));
