c84219451657d5a1fd7f5ca0db9c680b41cfb2eb
Marek Kobida <marek.kobida@gmail.com>
2021-08-18 10:31:41 +0200

    webpack (common)

--- webpack.js
+++ webpack.js
@@ -2,42 +2,8 @@
  * Copyright 2021 Marek Kobida
  */
 
-const Html = require('./webpack/Html');
-const path = require('path');
 const webpack = require('webpack');
 
-const INPUT_FILE_PATH = './private/index.tsx';
-
-const OUTPUT_FILE_NAME = 'index.js';
-
-const OUTPUT_FILE_PATH = './public';
-
-const compiler = webpack({
-  entry: INPUT_FILE_PATH,
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        type: 'asset/resource',
-      },
-      {
-        exclude: /node_modules/,
-        loader: './webpack/babel',
-        test: /\.tsx?$/,
-      },
-    ],
-  },
-  name: 'warden',
-  output: {
-    assetModuleFilename: '[name][ext]',
-    filename: OUTPUT_FILE_NAME,
-    path: path.resolve(OUTPUT_FILE_PATH),
-  },
-  plugins: [new Html()],
-  resolve: {
-    extensions: ['.js', '.json', '.ts', '.tsx'],
-  },
-});
+const compiler = webpack(require('./webpack/common')({ name: 'warden' }));
 
 compiler.watch({}, (_, __) => console.log(_, __?.toString({ colors: true })));
---
+++ webpack/common.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 Marek Kobida
+ */
+
+const Html = require('./Html');
+const path = require('path');
+const webpack = require('webpack');
+
+const INPUT_FILE_PATH = './private/index.tsx';
+
+const OUTPUT_FILE_NAME = 'index.js';
+
+const OUTPUT_FILE_PATH = './public';
+
+module.exports = function ({ assets, name }) {
+  return {
+    entry: INPUT_FILE_PATH,
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          type: 'asset/resource',
+        },
+        {
+          exclude: /node_modules/,
+          loader: './webpack/babel',
+          test: /\.tsx?$/,
+        },
+      ],
+    },
+    name,
+    output: {
+      assetModuleFilename: '[name][ext]',
+      filename: OUTPUT_FILE_NAME,
+      path: path.resolve(OUTPUT_FILE_PATH),
+    },
+    plugins: [new Html(assets)],
+    resolve: {
+      extensions: ['.js', '.json', '.ts', '.tsx'],
+    },
+  };
+};
