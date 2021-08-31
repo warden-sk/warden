2ea082446c329447356630fdb5ed68f650c7079b
Marek Kobida <marek.kobida@gmail.com>
2021-08-31 13:20:40 +0200

    update

--- private/index.tsx
+++ private/index.tsx
@@ -12,11 +12,11 @@ import ReactDOMServer from 'react-dom/server';
 function _1() {
   return (
     <div className="_1">
-      <div className="_2" p={2}>
-        <div className="_3" p={8}>
-          <h1 mB={4}>warden.sk</h1>
-          <p mB={4}>Čoskoro vám predstavíme stránku v úplne novom kabáte.</p>
-          <h2 mB={4}>Kontakt</h2>
+      <div className="_2" p="2">
+        <div className="_3" p="8">
+          <h1 mB="4">warden.sk</h1>
+          <p mB="4">Čoskoro vám predstavíme stránku v úplne novom kabáte.</p>
+          <h2 mB="4">Kontakt</h2>
           <p>
             Marek Kobida
             <br />
