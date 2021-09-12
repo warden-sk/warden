9c74af05eb693d61940859c8d21a8ad6a4af6117
Marek Kobida <marek.kobida@gmail.com>
2021-09-13 00:31:51 +0200

    update

--- private/index.tsx
+++ private/index.tsx
@@ -9,24 +9,7 @@ import ReactDOM from 'react-dom';
 import ReactDOMServer from 'react-dom/server';
 
 function _1() {
-  return (
-    <div className="_1">
-      <div alignItems="center" className="_2" display="flex" justifyContent="center" p="2">
-        <div className="_3" p="8">
-          <h1 mB="4">warden.sk</h1>
-          <p mB="4">Čoskoro vám predstavíme stránku v úplne novom kabáte.</p>
-          <h2 mB="4">Kontakt</h2>
-          <p>
-            Marek Kobida
-            <br />
-            <a href="tel:+421910123678">+421 910 123 678</a>
-            <br />
-            <a href="mailto:marek@warden.sk">marek@warden.sk</a>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+  return <div />;
 }
 
 typeof window !== 'undefined' && ReactDOM.render(<_1 />, document.getElementById('index'));
